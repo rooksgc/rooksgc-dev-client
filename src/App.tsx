0b70536df04567af5af485dc1e1f2091c9b55ada
aa.shestakov <aa.shestakov@tensor.ru>
2021-02-13 15:00:25 +0300

    adding sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import svelte from './svelte.png'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Коротко о Svelte</h1>
         <img src={svelte} className="App-logo" alt="logo" />
       </header>
     </div>
